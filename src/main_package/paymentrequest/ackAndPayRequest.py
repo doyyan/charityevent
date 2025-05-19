73f6e19f0ceb790cc9f965cf393d88d4a91bc902
Sivaraj, Ravikumar <Ravikumar.Sivaraj+adi@analog.com>
2025-05-19 22:09:02 +0100

    fix: correct email and remove hard coding

--- src/main_package/paymentrequest/ackAndPayRequest.py
+++ src/main_package/paymentrequest/ackAndPayRequest.py
@@ -24,17 +24,7 @@ def sendAckAndPayRequest(templateName, processedXlsFile):
         raise Exception(" File already open " + processedXlsFile)
 
     form = pd.read_excel(processedXlsFile)
-    adminEmail = "ravi.sivaraj@gmail.com"
-
-    # people_data = [
-    #     {"guestName": "Mrs Priya Ravikumar", "email": "priyaravikumar2000@yahoo.com", "adultNos": 2, "kidsNos": 2,
-    #      "voluntaryPrice": 100, "raffle": 5, "uniqueRef": "PR1"},
-    #     # {"guestName": "Jane Smith", "email": "ravi.sivaraj@gmail.com", "adultNos": 2, "kidsNos": 0,
-    #     #  "voluntaryPrice": 0, "uniqueRef": "JS001"},
-    #     # {"guestName": "Bob Johnson", "email": "ravi.sivaraj@gmail.com",
-    #     #  "adultNos": 0, "kidsNos": 2, "voluntaryPrice": 100, "uniqueRef": "JD001"
-    #     #  },
-    # ]
+    adminEmail = "dancebeatzedinburgh@gmail.com"
 
     emailHeaderField = 'Email'
     guestNameField = 'Guest name'
